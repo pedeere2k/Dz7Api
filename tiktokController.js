475ce3ec833e11c28fa8c297e61de4b77d979c7a
pdr2k <pedrooosantos2.13.3@gmail.com>
2026-02-06 20:06:15 -0300

    Delete tiktokController.js

--- tiktokController.js
+++
@@ -1,4 +0,0 @@
-const axios=require('axios');
-exports.tt=async(req,res)=>{try{const url=req.query.url;if(!url)return res.json({error:'url requerido'});
-const r=await axios.get(`https://api.tiklydown.me/api/download?url=${encodeURIComponent(url)}`);
-res.json({no_watermark:r.data.video.noWaterMark});}catch(e){res.json({error:e.message});}};
